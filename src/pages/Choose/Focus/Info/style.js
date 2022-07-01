7b38c28d2075d97f3fd0c83e5786fb44124ee264
sweet-mimosa <1284960976@qq.com>
2022-07-01 19:01:54 +0800

    修改了Info的样式

--- src/pages/Choose/Focus/Info/style.js
+++ src/pages/Choose/Focus/Info/style.js
@@ -1,10 +1,13 @@
 import styled from "styled-components";
 
 export const InfoWrapper = styled.div`
+    justify-content: center;
+    width: 90%;
     border-radius: 0.5rem;
     border: 1px solid gray;
     margin-top: 0.5rem;
     margin-bottom: 0.5rem;
+    margin-left: 0.5rem;
     .content {
         height: 2rem;
         font-size: 0.8rem;
